80678ac6376f2547e8efd7cc38791211bcced9f8
NeirouzJbira <jbira.neirouz@gmail.com>
2022-01-03 09:40:26 +0100

    init

---
+++ application/config/database.py
@@ -0,0 +1,10 @@
+# Database config. Used in various libraries.
+config = {
+    'driver': 'mysql',  # mongodb, mysql
+    'host': '127.0.0.1',
+    'port': 3306,
+    'db': 'flask_mvc_framework',
+    'auth': True,  # ignore for mysql
+    'user': 'tester',
+    'pw': 'password'
+}
