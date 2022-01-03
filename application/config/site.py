dc7e08e80fb784dd6f2537858af908c924a4009c
NeirouzJbira <jbira.neirouz@gmail.com>
2022-01-03 09:40:31 +0100

    init

---
+++ application/config/site.py
@@ -0,0 +1,7 @@
+# Can be called for easy loading of site vars
+defaults = {
+        'site': {
+                'title': 'Flask MVC Framework'
+        },
+        'meta_description': 'A starting point for Python Web Application using the Flask Microframework.'
+}
